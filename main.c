d28c9a028fd2fab5b175a539f28d88181f8a163b
Yuriy Osychenko <etoyura@gmail.com>
2016-02-14 17:35:18 +0300

    Add main fucntion and generated scanner

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "scanner.h"
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Program takes only one argument (path to source code file)\n");
+		return 1;
+	}
+
+	yyin = fopen(argv[1], "r");
+	
+	printf("Scanning file %s ...\n", argv[1]);
+	
+	yylex();
+
+	fclose(yyin);
+	
+	printf("Scanning is over\n");
+	
+	return 0;
+}
