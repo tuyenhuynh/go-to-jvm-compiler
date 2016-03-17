4f718a7cd4e42f1f0def900aa401a28093596ae1
Yuri Osychenko <etoyura@gmail.com>
2016-03-18 01:31:01 +0300

    Not fully implemented switch-stmt

--- print_trees.c
+++ print_trees.c
@@ -207,7 +207,6 @@ void expressionTypeToString(enum ExpressionType exprType, char* result) {
 }
 
 void printPrimaryExpression(int parentId, struct PrimaryExpression* primaryExpr) {}
-
 void print_declaration_list(int parentId, struct DeclarationList* program){}
 void print_declaration(int parentId, struct Declaration* declaration){}
 void print_import_statement_list(int parentId, struct ImportStmtList* importStmtList){}
@@ -219,17 +218,45 @@ void print_type_name(int parentId, struct Type* typeName){}
 void print_var_spec(int parentId, struct VarSpec* varSpec){}
 void print_var_spec_list(int parentId, struct VarSpecList* varSpecList){}
 void print_statement(int parentId, struct Statement* statement){}
-void print_simple_stmt(int parentId, struct SimpleStmt* simpleStmt){}
+void printSimpleStmt(int parentId, struct SimpleStmt* simpleStmt){}
 void print_return_stmt(int parentId, struct ReturnStmt* returnStmt){}
 void print_block(int parentId, struct Block* block){}
-void print_switch_stmt(int parentId, struct SwitchStmt* switchStmt){}
+
+
+void printSwitchStmt(int parentId, struct SwitchStmt* switchStmt) {
+	if (switchStmt != NULL) {
+		maxId++;
+		int id = maxId;
+		printSwitchInitialExpression(parentId, switchStmt->initialAndExpression);
+		id++;
+		printSwitchBody(id, switchStmt->switchBody);
+	}
+	else {
+		printf("Switch statement is null\n");
+	}
+}
+
+
 void print_if_stmt(int parentId, struct IfStmt* ifStmt){}
 void print_for_stmt(int parentId, struct ForStmt* forStmt){}
 void print_if_stmt_expression(int parentId, struct IfStmtExpression* ifStmtExpr){}
 void print_else_block(int parentId, struct ElseBlock* elseBlock){}
 void print_stmt_list(int parentId, struct StatementList* stmtList){}
-void print_switch_body(int parentId, struct SwitchBody* switchBody){}
-void print_expression_case_clause_list(int parentId, struct ExpressionCaseClauseList* eccl){}
+
+
+void printSwitchBody(int parentId, struct SwitchBody* switchBody) {
+	if (switchBody != NULL) {
+		maxId++;
+		printExpressionCaseClauseList(parentId, switchBody->eccl);
+	}
+	else {
+		printf("Switch body is null\n");
+	}
+
+}
+void printExpressionCaseClauseList(int parentId, struct ExpressionCaseClauseList* eccl) {
+	
+}
 void print_expression_case_clause(int parentId, struct ExpressionCaseClause* ecc){}
 void print_expression_switch_case(int parentId, struct ExpressionSwitchCase* expressionSwitchCase){}
 void print_for_clause(int parentId, struct ForClause* ForClause){}
@@ -239,7 +266,16 @@ void print_param_list(int parentId, struct ParameterList* paramList){}
 void print_param_declare(int parentId, struct ParameterDeclare* paramDeclare){}
 void print_result(int parentId, struct Result* result){}
 void print_function_call(int parentId, struct FunctionCall* functionCall){}
-void print_switch_initial_expression(int parentId, struct SwitchInitialAndExpression* switchInitialAndExpression){}
+
+void printSwitchInitialExpression(int parentId, struct SwitchInitialAndExpression* switchInitialAndExpression){
+	if (switchInitialAndExpression != NULL) {
+		maxId++;
+		printExpression(parentId, switchInitialAndExpression->expression);
+		printSimpleStmt(parentId, switchInitialAndExpression->initialStmt);
+	}
+
+
+}
 void print_for_init_stmt(int parentId, struct ForInitStmt* forInitStmt){}
 void print_for_condition(int parentId, struct ForCondition* forCondition){}
 void print_for_post_stmt(int parentId, struct ForPostStmt* forPostStmt){}
