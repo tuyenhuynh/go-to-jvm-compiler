33911300bc42cbe6f2e2a2bd564159f6b329ad7f
tuyenhuynh <tuyenhm93@gmail.com>
2016-03-25 23:56:26 +0300

    Added implementation for checkPrimaryType

--- semantic.c
+++ semantic.c
@@ -1,4 +1,5 @@
 #include "semantic.h"
+#define NULL 0
 
 struct SemanticType* checkExpressionType(struct Expression* expr) {
 	struct SemanticType* type = (struct SemanticType*) malloc(sizeof(struct SemanticType));
@@ -12,23 +13,61 @@ struct SemanticType* checkExpressionType(struct Expression* expr) {
 }
 
 struct SemanticType* checkPrimaryExpressionType(struct PrimaryExpression* primaryExpr) {
-	struct SemanticType* type = (struct SemanticType*) malloc(sizeof(struct SemanticType));
-
-	switch (primaryExpr->exprType)
-	{
-		case BOOL_TRUE_EXPRESSION:
-		case BOOL_FALSE_EXPRESSION: {
-			type->typeName = BOOL_TYPE_NAME; 
-
-		}
-		case DECIMAL_EXPR: {
-			type->typeName = INT_TYPE_NAME; 
-		}
-		case ID_EXPRESSION: {
-			//??? how to get the semantic type of and declared id ? from constant table ???
-		}
-		default:
-			break;
+	struct SemanticType* type = (struct SemanticType*)malloc(sizeof(struct SemanticType));
+	//type->IS_ARRAY_ACCESS = false; 
+	switch (primaryExpr->exprType) {
+	case BOOL_TRUE_EXPRESSION:
+	case BOOL_FALSE_EXPRESSION: {
+		//TODO: implement this
+		break;
 	}
-	return type; 
+	case DECIMAL_EXPR: {
+		type->typeName = INT_TYPE_NAME;
+		break;
+	}
+	case FLOAT_EXPR: {
+		type->typeName = FLOAT32_TYPE_NAME;
+		break;
+	}
+	case STRING_EXPR: {
+		type->typeName = STRING_TYPE_NAME;
+		break;
+	}
+	case ID_EXPRESSION: {
+		type->typeName = IDENTIFIER_TYPE_NAME;
+		//add id to table of variable
+		break;
+	}
+	case EXPRESSION: {
+		//how to eliminate multilevel expression ???
+		struct SemanticType* nestedType = checkExpressionType(primaryExpr->expr);
+		type = nestedType;
+		break;
+	}
+	case PE_COMPOSITE: {
+		struct SemanticType* leftExpr = checkExpressionType(primaryExpr->primaryExpr->expr);
+		struct SemanticType* rightExpr = checkExpressionType(primaryExpr->expr);
+		if (leftExpr->typeName != IDENTIFIER_TYPE_NAME) {
+			printf("Semantic error. Invalid array name type \n");
+			type = NULL;
+		}
+		if (rightExpr->typeName != INT_TYPE_NAME) {
+			printf("Semantic error. Index must be integer type \n"); 
+			type = NULL;
+		}
+		else {
+			//if identifier and expression type (int) is ok, how to know type of array access ???
+			//from table of variables ???
+			//TODO: define the type of this expression
+		}
+		break;
+	}
+	case FUNCTION_CALL:
+	case FUNCTION_CALL_EMPTY: {
+		//TODO: search the definition of function in constant table to find return type 
+		break;
+	}
+	}
+	primaryExpr->semanticType = type;
+	return type;
 }
