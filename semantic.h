909423b97cae370244cb8c3bee3536f5171cf44f
tuyenhuynh <tuyenhm93@gmail.com>
2016-03-26 02:01:19 +0400

    Added checking type need to be implemented

--- semantic.h
+++ semantic.h
@@ -14,5 +14,19 @@ bool checkSemanticElseBlock(struct ElseBlock* elseBlock, char* functionName);
 bool checkSemanticBlock(struct Block* block, char* functionName); 
 bool checkSemanticSwitchStmt(struct SwitchStmt* switchStmt, char* functionName);
 bool checkSemanticForStmt(struct ForStmt* forStmt, char* functionName ); 
-
+bool checkSemanticFunction(struct FunctionDecl* functionDecl);
+bool checkSemanticSignature(struct Signature* signature);
+bool checkSemanticParamList(struct ParameterList* paramList); 
+bool checkSemanticReturnType(struct Result* result); 
+bool checkSemanticPrintStmt(struct PrintStatement* printStmt, char* functionName);
+bool checkSemanticSancStmt(struct ScanStatement* scanStmt, char* functionName); 
+bool checkSemanticReturnStmt(struct ReturnStmt* returnStmt); 
+bool checkSemanticFunctionCall(struct ExpressionList* exprList, struct ParameterList* paramList, char* functionName);
+bool checkSemanticVarDecl(struct VarDecl* varDecl, char* functionName);
+bool checkSemanticConstDecl(struct ConstDecl* constDecl, char* functionName); 
+bool checkSemanticVarSpec(struct VarSpec* varSpec, char* functionName);
+bool checkSemanticStmt(struct Statement* statement, char* functionName);
+bool checkSemanticSimpleStmt(struct SimpleStmt* simpleStmt, char* functionName);
+bool checkSemanticAssignStmtList(struct ExpressionList* leftExprList, struct ExpressionList* rightExprList, char* functionName);
+bool checkSemanticAssignStmt(struct Expression* leftExpr, struct Expression* rightExpr, char* functionName);
 #endif //_SEMANTIC_H_
